a7032b7da8431979d0815d72fd04f283dc52dd04
Haydn <hvannorm@asu.edu>
2022-11-20 18:46:16 -0700

    second

--- spotifyPlaylist.py
+++ spotifyPlaylist.py
@@ -15,3 +15,26 @@ playlist_name = "Weather"
 playlist_description = "day"
 
 spotifyObject.user_playlist_create(user = username,name = playlist_name, public = True ,description = playlist_description)
+
+lim = 20
+#change rec genre list
+recc = spotifyObject.recommendations(seed_genres=["rainy-day","rock"],limit=lim)
+#print(json.dumps(recc,sort_keys=4,indent=4))
+
+g = spotifyObject.recommendation_genre_seeds()
+print(json.dumps(g,sort_keys=4,indent=4))
+#the one that works index on the 0 and get uri to add to playlist
+for x in range(lim):
+	print(recc["tracks"][x]["name"])
+
+
+
+user_input = (input('name of song:'))
+while user_input != 'quit':
+	result = spotifyObject.search(q=user_input,limit=1)
+	print("\n\n\n")
+	print(json.dumps(result,sort_keys=4,indent=4))
+	track1 = result['tracks']['items'][0]['uri']
+	#print(spotifyObject.audio_features(track1))
+	#print(result["tracks"])
+	user_input = input('name of song:')
